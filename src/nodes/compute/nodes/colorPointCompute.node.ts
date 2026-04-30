97bf899e4efda44a8964d8bbc50b733e2baca1c5
David Johnston <david@blacksheepcode.com>
2026-04-30 13:53:28 +1000

    Migrate color point

---
+++ src/nodes/compute/nodes/colorPointCompute.node.ts
@@ -0,0 +1,36 @@
+import { defineComputeNode, } from '../types';
+
+export const colorPointNodeDef = defineComputeNode('colorPointCompute', {
+  defaults: {
+    "color": {
+      "v": {
+        "a": 1,
+        "r": 1,
+        "g": 1,
+        "b": 1
+      },
+
+    },
+    point: {
+      v: {
+        x: 0,
+        y: 0
+      }
+    }
+  },
+
+  evaluate(inputs) {
+    const point = inputs.point;
+    const color = inputs.color;
+    return {
+      colorPoint: {
+        x: point.v.x,
+        y: point.v.y,
+        r: color.v.r,
+        g: color.v.g,
+        b: color.v.b,
+        a: color.v.a,
+      },
+    };
+  },
+});
--- src/nodes/compute/registry.ts
+++ src/nodes/compute/registry.ts
@@ -1,18 +1,18 @@
 import type { NodeDef } from './types';
-import { timeNodeDef }           from './nodes/time.node';
-import { orbitNodeDef }          from './nodes/orbit.node';
-import { colorPointNodeDef }     from './nodes/colorPoint.node';
-import { waveNodeDef }           from './nodes/wave.node';
-import { pointsOnALineNodeDef }  from './nodes/pointsOnALine.node';
-import { multiplierNodeDef }     from './nodes/multiplier.node';
-import { addNodeDef }            from './nodes/add.node';
+import { timeNodeDef } from './nodes/time.node';
+import { orbitNodeDef } from './nodes/orbit.node';
+import { colorPointNodeDef } from './nodes/colorPointCompute.node';
+import { waveNodeDef } from './nodes/wave.node';
+import { pointsOnALineNodeDef } from './nodes/pointsOnALine.node';
+import { multiplierNodeDef } from './nodes/multiplier.node';
+import { addNodeDef } from './nodes/add.node';
 
 export const computeRegistry = new Map<string, NodeDef>([
-  [timeNodeDef.type,          timeNodeDef],
-  [orbitNodeDef.type,         orbitNodeDef],
-  [colorPointNodeDef.type,    colorPointNodeDef],
-  [waveNodeDef.type,          waveNodeDef],
+  [timeNodeDef.type, timeNodeDef],
+  [orbitNodeDef.type, orbitNodeDef],
+  [colorPointNodeDef.type, colorPointNodeDef],
+  [waveNodeDef.type, waveNodeDef],
   [pointsOnALineNodeDef.type, pointsOnALineNodeDef],
-  [multiplierNodeDef.type,    multiplierNodeDef],
-  [addNodeDef.type,           addNodeDef],
+  [multiplierNodeDef.type, multiplierNodeDef],
+  [addNodeDef.type, addNodeDef],
 ]);
